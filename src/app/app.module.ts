9df466c148dfd6033fdf0a4801107725dae4966c
JamesCCoder <jameswu.java@gmail.com>
2022-02-02 21:51:32 -0600

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
-@NgModule({
+@NgModule({ 
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
