4af09355b7b12046fe281f9ea5089b286503cf38
JamesCCoder <jameswu.java@gmail.com>
2022-02-03 09:46:33 -0600

    3 files modified

--- src/server/server.component.ts
+++ src/server/server.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: '[server-app]',
+  selector: 'server-app',
   templateUrl: './server.component.html',
   styleUrls: [ './server.component.css' ]
 })
 export class ServerComponent  {
-
+  word : String = "test interpolation";
+  sentence : String = "This is a sentence";
 }
